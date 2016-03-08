8c3b665f7d15ee28b16fa6484dbc98b06857fda2
Dmitry <matrix.predictor@gmail.com>
2016-03-08 11:40:07 +0500

    Пример для brain.js, который мне понятен, и который не использую и пример с convnetjs, который не понятен и который буду использовать.

---
+++ sandbox/brain_example.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Дмитрий on 23.11.2015.
+ */
+var brain = require('brain');
+var net = new brain.NeuralNetwork();
+
+net.train([{input: [0, 0], output: [0]},
+   {input: [0, 1], output: [1]},
+   {input: [1, 0], output: [1]},
+   {input: [1, 1], output: [0]}]);
+
+var output = net.run([1, 0]);  // [0.987]
+console.log(output);
---
+++ sandbox/conv_net_js_example.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Дмитрий on 03.03.2016.
+ */
+var convnetjs = require("convnetjs");
+
+var layer_defs = [];
+// minimal network: a simple binary SVM classifer in 2-dimensional space
+layer_defs.push({type:'input', out_sx:1, out_sy:1, out_depth:2});
+layer_defs.push({type:'svm', num_classes:2});
+
+// create a net
+var net = new convnetjs.Net();
+net.makeLayers(layer_defs);
+
+// create a 1x1x2 volume of input activations:
+var x = new convnetjs.Vol(1,1,2);
+x.w[0] = 0.5; // w is the field holding the actual data
+x.w[1] = -1.3;
+// a shortcut for the above is var x = new convnetjs.Vol([0.5, -1.3]);
+
+var scores = net.forward(x); // pass forward through network
+// scores is now a Vol() of output activations
+console.log('score for class 0 is assigned:'  + scores.w[0]);
