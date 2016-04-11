5f4e45c202aeec26303be7f544dce1ed20bfcee0
matrix.predictor <matrix.predictor@gmail.com>
2016-04-11 06:26:05 +0500

    Незначитальные изменения в скрипте загрузки

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 //http://api.kinopoisk.cf/getReviews?filmID=326&page=27
+//http://api.kinopoisk.cf/getReviews?filmID=93377&page=27
 
 var request = require('request'),
    fs = require('fs'),
    url = 'http://api.kinopoisk.cf/getReviews?filmID=',
    pageParams = '&page=',
-   reviews = [];
+   reviews = [],
+   id = 93377;
 /*
 * Функция первого запроса. 
 *
@@ -16,35 +18,40 @@ function getFirstReviewsPageOnFilm(id){
          var firstResponseObject = JSON.parse(body);
          // Если по данному фильму есть отзывы, то стянем все отзывы
          if(typeof firstResponseObject.reviews !== 'undefined'){
-            var reviewAllCount = firstResponseObject.reviewAllCount;
-            reviews.concat(firstResponseObject.reviews);
-            getReviewsPage(2, reviewAllCount);
+            var pagesCount = firstResponseObject.pagesCount;
+            reviews = reviews.concat(firstResponseObject.reviews);
+            console.log(reviews);
+            // getReviewsPage(2, pagesCount);
          }
       }
    });
 }
 
-function getReviewsPage(pageNumber, reviewAllCount) {
-   if (pageNumber <= reviewAllCount) {
+function getReviewsPage(pageNumber, pagesCount) {
+   if (pageNumber <= pagesCount) {
       request(url + id + '&page=' + pageNumber, function (error, response, body) {
          if (!error && response.statusCode == 200) {
             console.log('ok, status 200');
             var responseObject = JSON.parse(body);
-            reviews.concat(responseObject.reviews);
-            getReviewsPage(pageNumber + 1, reviewAllCount)
+            reviews = reviews.concat(responseObject.reviews);
+            getReviewsPage(pageNumber + 1, pagesCount)
          }
          else {
             console.log('error')
          }
       });
    } else {
-      console.log('all page load')
+      console.log('all page load');
+      writeResult(reviews);
    }
 }
 
 function writeResult (data){
+   data = JSON.stringify(data);
    fs.writeFile('data.json', data, function(err){
       if (err) throw err;
       console.log('It\'s saved!');
    });
 }
+
+getFirstReviewsPageOnFilm(93377);
